8a45f84a3886e7b43b363fb863383fc947f54ea7
GBTC440011UR <GBTC440011UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-02-20 16:13:22 -0500

    .

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,51 +1,86 @@
 package com.example.demo;
 
-
 import com.cloudinary.utils.ObjectUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.validation.Valid;
 import java.io.IOException;
 import java.util.Map;
+
 
 @Controller
 public class HomeController {
     @Autowired
-   FoodRepository foodRepository;
+    Repository repository;
 
     @Autowired
     CloudinaryConfig cloudc;
 
     @RequestMapping("/")
-    public String listFoods(Model model) {
-        model.addAttribute("foods", foodRepository.findAll());
+    public String listItems(Model model){
+        model.addAttribute("todos", repository.findAll());
         return "index";
     }
 
     @GetMapping("/add")
-    public String newFood(Model model) {
-        model.addAttribute("food", new Food());
+    public String todoForm(Model model){
+        model.addAttribute("todo", new Todo());
         return "form";
     }
 
+
+
     @PostMapping("/add")
-    public String processFood(@ModelAttribute Food food,
-                               @RequestParam("file") MultipartFile file) {
+    public String processTood(@ModelAttribute Todo todo,
+                              @RequestParam("file") MultipartFile file) {
         if (file.isEmpty()) {
             return "redirect:/add";
         }
         try {
             Map uploadResult = cloudc.upload(file.getBytes(),
                     ObjectUtils.asMap("resourcetype", "auto"));
-            food.setPictureURL(uploadResult.get("url").toString());
-            foodRepository.save(food);
+            todo.setPicture(uploadResult.get("url").toString());
+            repository.save(todo);
         } catch (IOException e) {
             e.printStackTrace();
             return "redirect:/add";
         }
+        return "redirect:/";
+    }
+
+
+
+    @PostMapping("/process")
+    public String processForm(@Valid Todo todo, BindingResult result){
+        if (result.hasErrors()){
+            return "form";
+        }
+        repository.save(todo);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/detail/{id}")
+    public String showCourse(@PathVariable("id") long id, Model model)
+    {
+        model.addAttribute("todo", repository.findById(id).get());
+        return "show";
+    }
+
+    @RequestMapping("/update/{id}")
+    public String updateTodo(@PathVariable("id") long id,
+                             Model model){
+        model.addAttribute("todo", repository.findById(id).get());
+        return "form";
+    }
+
+    @RequestMapping("/delete/{id}")
+    public String delTodo(@PathVariable("id") long id){
+        repository.deleteById(id);
         return "redirect:/";
     }
 
--- src/main/java/com/example/demo/Repository.java
+++ src/main/java/com/example/demo/Repository.java
@@ -2,5 +2,5 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface FoodRepository extends CrudRepository<Food, Long> {
+public interface Repository extends CrudRepository<Todo, Long> {
 }
