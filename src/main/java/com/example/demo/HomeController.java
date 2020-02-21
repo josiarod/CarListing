88cfcc4842103717bfe90fd80d5d74afc36681a4
Josia <josiarodriguez@gmail.com>
2020-02-21 16:49:24 -0500

    .

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -8,6 +8,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+
 import javax.validation.Valid;
 import java.io.IOException;
 import java.util.Map;
@@ -18,68 +19,48 @@ public class HomeController {
     @Autowired
     Repository repository;
 
-    @Autowired
-    CloudinaryConfig cloudc;
-
     @RequestMapping("/")
     public String listItems(Model model){
-        model.addAttribute("todos", repository.findAll());
+        model.addAttribute("jobs", repository.findAll());
         return "index";
     }
 
     @GetMapping("/add")
-    public String todoForm(Model model){
-        model.addAttribute("todo", new Todo());
+    public String jobForm(Model model){
+        model.addAttribute("job", new Job());
         return "form";
     }
 
 
 
-    @PostMapping("/add")
-    public String processTood(@ModelAttribute Todo todo,
-                              @RequestParam("file") MultipartFile file) {
-        if (file.isEmpty()) {
-            return "redirect:/add";
-        }
-        try {
-            Map uploadResult = cloudc.upload(file.getBytes(),
-                    ObjectUtils.asMap("resourcetype", "auto"));
-            todo.setPicture(uploadResult.get("url").toString());
-            repository.save(todo);
-        } catch (IOException e) {
-            e.printStackTrace();
-            return "redirect:/add";
-        }
-        return "redirect:/";
-    }
 
 
 
     @PostMapping("/process")
-    public String processForm(@Valid Todo todo, BindingResult result){
+    public String processForm(@Valid Job job, BindingResult result){
         if (result.hasErrors()){
             return "form";
         }
-        repository.save(todo);
+        repository.save(job);
         return "redirect:/";
     }
 
     @RequestMapping("/detail/{id}")
-    public String showCourse(@PathVariable("id") long id, Model model)
+    public String showJob(@PathVariable("id") long id, Model model)
     {
-        model.addAttribute("todo", repository.findById(id).get());
+        model.addAttribute("job", repository.findById(id).get());
         return "show";
     }
 
     @RequestMapping("/update/{id}")
     public String updateTodo(@PathVariable("id") long id,
                              Model model){
-        model.addAttribute("todo", repository.findById(id).get());
+        model.addAttribute("job", repository.findById(id).get());
         return "form";
     }
 
     @RequestMapping("/delete/{id}")
-    public String delTodo(@PathVariable("id") long id){
+    public String delJob(@PathVariable("id") long id){
         repository.deleteById(id);
         return "redirect:/";
     }
--- src/main/java/com/example/demo/Repository.java
+++ src/main/java/com/example/demo/Repository.java
@@ -2,5 +2,5 @@ package com.example.demo;
 
 import org.springframework.data.repository.CrudRepository;
 
-public interface Repository extends CrudRepository<Todo, Long> {
+public interface Repository extends CrudRepository<Job, Long> {
 }
